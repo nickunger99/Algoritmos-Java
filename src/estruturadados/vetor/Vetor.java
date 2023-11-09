728eb3b59c9100852708176fdd39afeef3446f2c
nickunger99 <nicolas.unger@yahoo.com.br>
2023-11-09 14:08:07 -0300

    Vetor - toString

--- src/estruturadados/vetor/Vetor.java
+++ src/estruturadados/vetor/Vetor.java
@@ -1,12 +1,57 @@
 package estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
-    public Vetor(int capacidade){
+    public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
         System.out.println("capacidade do vetor: " + elementos.length);
     }
 
+//    public void adiciona(String elemento){
+//        for (int i=0; i<this.elementos.length; i++){
+//            if(this.elementos[i] == null){
+//                this.elementos[i] = elemento;
+//                System.out.println(this.elementos[i]);
+//                break;
+//            }
+//        }
+//    }
+
+    public boolean adiciona(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            System.out.println(this.elementos[tamanho]);
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+        s.append("]");
+
+
+        return s.toString();
+    }
 }
--- src/estruturadados/vetor/teste/VetorTeste.java
+++ src/estruturadados/vetor/teste/VetorTeste.java
@@ -5,5 +5,16 @@ import estruturadados.vetor.Vetor;
 public class VetorTeste {
     public static void main(String[] args) {
         Vetor vetor = new Vetor(20);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+        vetor.adiciona("elemento 4");
+        vetor.adiciona("elemento 5");
+        vetor.adiciona("elemento 6");
+
+        System.out.println(vetor.tamanho());
+
+        System.out.println(vetor);
     }
 }
